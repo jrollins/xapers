f4b705ea9bde0f95d9f8dee78c08b7ddf41e2e31
Jameson Graef Rollins <jrollins@finestructure.net>
2015-11-14 13:45:29 -0800

    source: add scan_regex for arXiv source

--- lib/xapers/sources/arxiv.py
+++ lib/xapers/sources/arxiv.py
@@ -10,6 +10,9 @@ url_format = 'http://arxiv.org/abs/%s'
 
 url_regex = 'http://arxiv.org/(?:abs|pdf|format)/([^/]*)'
 
+# http://arxiv.org/help/arxiv_identifier
+scan_regex = 'arXiv:([0-9]{4}\.[0-9]{4,5})(?:v[0-9]+)?'
+
 # html parser override to override handler methods
 class MyHTMLParser(HTMLParser):
     def __init__(self):
