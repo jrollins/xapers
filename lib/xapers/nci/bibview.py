728e3884d209567eadfd03df09df409f6aec9ede
Jameson Graef Rollins <jrollins@finestructure.net>
2016-12-23 21:07:15 -0800

    nci: using existing ui.db when retrieving bibtex

--- lib/xapers/nci/bibview.py
+++ lib/xapers/nci/bibview.py
@@ -1,7 +1,5 @@
 import urwid
 
-from ..cli import initdb
-
 ############################################################
 
 class Bibview(urwid.Frame):
@@ -14,14 +12,14 @@ class Bibview(urwid.Frame):
 
         string = ''
 
-        with initdb() as db:
-            if db.count(query) == 0:
-                self.ui.set_status('No documents found.')
-            else:
-                for doc in db.search(query, limit=20):
-                    bibtex = doc.get_bibtex()
-                    if bibtex:
-                        string = string + bibtex + '\n'
+        db = self.ui.db
+        if db.count(query) == 0:
+            self.ui.set_status('No documents found.')
+        else:
+            for doc in db.search(query, limit=20):
+                bibtex = doc.get_bibtex()
+                if bibtex:
+                    string = string + bibtex + '\n'
 
         body = urwid.Filler(urwid.Text(string))
 
